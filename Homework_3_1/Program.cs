a6ff3dec9fe90066bf95d2b37ad066eb9b5087a8
ReneeShkipper <marinkagnome@gmail.com>
2023-10-30 17:19:04 +0300

    Add project files.

---
+++ Homework_3_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Input B");
+int B = int.Parse(Console.ReadLine());
+for (int i = A; i > B-1; i--)    Console.WriteLine(i);
+Console.ReadLine();
